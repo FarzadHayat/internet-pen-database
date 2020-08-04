b5a4c03ec0e5189f18860bbc56ba769102dfdcbf
Farzad <16559@burnside.school.nz>
2020-08-05 09:40:08 +1200

    All brands cards: Added hover for info with a read more link.

---
+++ static/javascript/script.js
@@ -0,0 +1,11 @@
+shortify = () =>{
+    list = document.getElementsByClassName("preview-text");
+    console.log(list)
+    // para.innerText = para.innerText.substr(0, 100) + '...';
+    var i;
+    for (i = 0; i < list.length; i++) {
+        console.log("hello")
+    }
+}
+
+shortify();
