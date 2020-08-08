b76dbd69c97852aea8421ad344d2dbf5e58f7de4
Farzad <16559@burnside.school.nz>
2020-08-08 20:54:40 +1200

    Added javascript for squarifying all brands cards. (uses jQuery to find cards and set card height equal to card width)

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,11 +1,30 @@
-shortify = () =>{
-    list = document.getElementsByClassName("preview-text");
-    console.log(list)
-    // para.innerText = para.innerText.substr(0, 100) + '...';
-    var i;
-    for (i = 0; i < list.length; i++) {
-        console.log("hello")
+// Resize all brand cards into squares by setting the height to equal the width.
+squarify = () =>{
+    var cardList = document.getElementsByClassName('card');
+    for (var i = 0; i < cardList.length; i++ ) {
+        var cardWidth = $(cardList[i]).width();
+        $(cardList[i]).css({'height':cardWidth+'px'});
     }
 }
 
-shortify();
+// Squarify to set the intial size of the cards.
+window.onload = () =>{
+    squarify();
+}
+
+// Squarify every time the window is resized.
+window.onresize = () =>{
+    squarify();
+}
+
+// shortify = () =>{
+//     textList = document.getElementsByClassName("preview-text");
+//     console.log(list)
+//     // para.innerText = para.innerText.substr(0, 100) + '...';
+//     var i;
+//     for (i = 0; i < list.length; i++) {
+//         console.log("hello")
+//     }
+// }
+
+// shortify();
